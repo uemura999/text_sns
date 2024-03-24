bcc943d881d426f14a81f84f4c7ee7f2faf69f96
uemura999 <koror99espana@gmail.com>
2024-03-25 04:33:42 +0900

    CloudFirestoreに最初のデータを保存しよう

--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import '../flavors.dart';
 
@@ -9,6 +10,23 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(F.title),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          try {
+            final firstData = {"msg": "Success"};
+            await FirebaseFirestore.instance
+                .collection('data')
+                .doc()
+                .set(firstData);
+
+            debugPrint("Successfully added data");
+          } catch (e) {
+            debugPrint("The access was denied: $e");
+          }
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
       ),
       body: Center(
         child: Text(
