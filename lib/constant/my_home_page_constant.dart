c4d684b675b3c4871419340f1183df47649dd5b9
uemura999 <koror99espana@gmail.com>
2024-03-27 20:30:55 +0900

    Constantフォルダを作成しよう

---
+++ lib/constant/my_home_page_constant.dart
@@ -0,0 +1,5 @@
+class MyHomePageConstant {
+  static const createUserFailureMsg = 'Failed to create user';
+  static const readUserSuccessMsg = 'Successfully read user';
+  static const readUserFailureMsg = 'Failed to read user';
+}
