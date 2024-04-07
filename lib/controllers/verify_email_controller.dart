7dcf3e7a7aa6d1defce07b5f3618d766280b5b6f
uemura999 <koror99espana@gmail.com>
2024-04-08 02:13:45 +0900

    メールアドレスを認証するControllerを作成しよう

---
+++ lib/controllers/verify_email_controller.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+import 'package:text_sns/controllers/auth_controller.dart';
+import 'package:text_sns/repository/auth_repository.dart';
+import 'package:text_sns/ui_core/ui_helper.dart';
+
+class VerifyEmailController extends GetxController {
+  Timer? timer;
+  @override
+  void onInit() async {
+    final user = AuthController.to.rxAuthUser.value;
+    if (user == null) return;
+    await _sendEmailVerification(user);
+    super.onInit();
+  }
+
+  Future<void> _sendEmailVerification(User user) async {
+    final repository = AuthRepository();
+    final result = await repository.sendEmailVerification(user);
+    result.when(success: (_) {
+      _startTimer(user);
+      UIHelper.showFlutterToast("Sent verification email");
+    }, failure: () {
+      UIHelper.showFlutterToast("Failed to send verification email");
+    });
+  }
+
+  void _startTimer(User user) {
+    timer = Timer.periodic(const Duration(seconds: 1), (_) async {
+      //1秒ごとに行いたい処理
+      await user.reload(); //メールが検証されたかどうかを確認するためにリロード
+      final newUser = FirebaseAuth.instance.currentUser;
+      if (newUser!.emailVerified) {
+        AuthController.to.rxAuthUser.value = newUser;
+        timer?.cancel();
+      }
+    });
+  }
+}
--- lib/view/pages/my_home_page/components/verify_email_screen.dart
+++ lib/view/pages/my_home_page/components/verify_email_screen.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
+import 'package:text_sns/controllers/verify_email_controller.dart';
 
 class VerifyEmailScreen extends StatelessWidget {
   const VerifyEmailScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Get.put(VerifyEmailController());
     const style = TextStyle(fontSize: 40.0);
     return const Center(
       child: Text('Verify email address', style: style),
