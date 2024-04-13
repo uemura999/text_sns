d91c5eeabb85f5b92dce13b96faea8b17f410f9f
uemura999 <koror99espana@gmail.com>
2024-04-13 20:03:58 +0900

    公開ユーザーを作成しよう

--- lib/view/pages/my_home_page/components/main_screen.dart
+++ lib/view/pages/my_home_page/components/main_screen.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:text_sns/controllers/auth_controller.dart';
+import 'package:text_sns/controllers/main_controller.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
   @override
   Widget build(BuildContext context) {
+    final mainController = Get.put(MainController());
     final authController = AuthController.to;
     const style = TextStyle(fontSize: 30.0);
-    return ElevatedButton(
-        onPressed: authController.onSignOutButtonPressed,
-        child: const Text(
-          "Sign out",
-          style: style,
-        ));
+    return Column(
+      children: [
+        Obx(
+          () => Text(
+            mainController.rxPublicUser.value?.uid ?? "Null",
+            style: style,
+          ),
+        ),
+        const SizedBox(height: 20.0),
+        ElevatedButton(
+            onPressed: authController.onSignOutButtonPressed,
+            child: const Text(
+              "Sign out",
+              style: style,
+            )),
+      ],
+    );
   }
 }
