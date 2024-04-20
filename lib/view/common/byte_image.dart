7fbe5586d1d71ae35c0adb39e4b75b3dcd4d0f19
uemura999 <koror99espana@gmail.com>
2024-04-21 01:43:44 +0900

    画像をアップロードしてユーザー情報を更新しよう

---
+++ lib/view/common/byte_image.dart
@@ -0,0 +1,18 @@
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+
+class ByteImage extends StatelessWidget {
+  const ByteImage({super.key, required this.bytes});
+  final Uint8List bytes;
+  @override
+  Widget build(BuildContext context) {
+    return ClipOval(
+      child: SizedBox(
+        width: 60.0,
+        height: 60.0,
+        child: Image.memory(bytes, fit: BoxFit.cover),
+      ),
+    );
+  }
+}
--- lib/view/pages/my_home_page/components/main_screen/components/content_screen.dart
+++ lib/view/pages/my_home_page/components/main_screen/components/content_screen.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 import 'package:text_sns/controllers/auth_controller.dart';
 import 'package:text_sns/controllers/main_controller.dart';
-import 'package:text_sns/enums/env_key.dart';
 
 class ContentScreen extends StatelessWidget {
   const ContentScreen({super.key});
@@ -20,10 +18,7 @@ class ContentScreen extends StatelessWidget {
           ),
         ),
         const SizedBox(height: 20.0),
-        Text(
-          dotenv.get(EnvKey.AWS_S3_USER_IMAGES_BUCKET.name),
-          style: style,
-        ),
+        // ByteImage(bytes: bytes), TODO:
         const SizedBox(height: 20.0),
         ElevatedButton(
             onPressed: authController.onSignOutButtonPressed,
