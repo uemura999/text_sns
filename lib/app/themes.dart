96ad1ac9ada334b9b53ca9d6d757aac6cb04e33b
uemura999 <koror99espana@gmail.com>
2024-04-21 03:08:34 +0900

    色を整えよう

--- lib/app/themes.dart
+++ lib/app/themes.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:text_sns/constant/colors.dart';
 
 class Themes {
-  static ThemeData lightTheme() => ThemeData.light()
-      .copyWith(appBarTheme: const AppBarTheme(color: Colors.green));
+  static ThemeData lightTheme() => ThemeData.light().copyWith(
+        primaryColor: kPrimaryColorLightTheme,
+        scaffoldBackgroundColor: kContentColorLightTheme,
+        appBarTheme: const AppBarTheme(color: kPrimaryColorLightTheme),
+      );
   static ThemeData darkTheme() => ThemeData.dark().copyWith(
-        appBarTheme: const AppBarTheme(color: Colors.black),
+        primaryColor: kPrimaryColorDarkTheme,
+        scaffoldBackgroundColor: kContentColorDarkTheme.withOpacity(0.87),
+        appBarTheme: const AppBarTheme(color: kPrimaryColorDarkTheme),
       );
 }
